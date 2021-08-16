2f44d12e7aeca1482dddb4ef5ca79d0e789cd7d4
427harinn <85378204+427harinn@users.noreply.github.com>
2021-08-16 17:12:39 +0900

    ガチャ

---
+++ OG3/Assets/Scripts/gachasclipt/buttonsclipt.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonsclipt : MonoBehaviour
+{
+    [SerializeField] GameObject resultpanel;
+    [SerializeField] GameObject faImage;
+    public int time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(resultpanel.activeSelf == true)
+        {
+            time++;
+        }
+        if(time > 350)
+        {
+            faImage.SetActive(false);
+        }
+    }
+    public void onClicked_resultreturn()
+    {
+        if(time > 400)
+        {
+            resultpanel.SetActive(false);
+            time = 0;
+        }
+    }
+
+
+}
---
+++ OG3/Assets/Scripts/gachasclipt/gachasclipt.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gachasclipt : MonoBehaviour
+{
+    int value;
+    public Sprite bearsprite;
+    public Sprite ringsprite;
+    //public GameObject result;
+    private Text _result;
+    [SerializeField] GameObject resultpanel;
+    AudioSource fa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultpanel.SetActive(false);
+        fa = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClicked_gacha()
+    {
+        fa.Play();
+        resultpanel.SetActive(true);
+        value = Random.Range(0, 2);
+        Image resultimage = (Image)GameObject.Find("resultImage").GetComponent<Image>();
+        _result = GameObject.Find("resulttext").GetComponent<Text>();
+        if (value == 0)
+        {
+            resultimage.sprite = bearsprite;
+            _result.text = "ぬいぐるみゲット！";
+        }
+        else if (value == 1)
+        {
+            resultimage.sprite = ringsprite;
+            _result.text = "指輪ゲット！";
+        }
+    }
+}
