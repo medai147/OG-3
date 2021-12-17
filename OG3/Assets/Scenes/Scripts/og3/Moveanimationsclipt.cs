d556e8ec03773d4edae1842b59fd2139a4de946c
Haruka　Karakawa <85378204+427harinn@users.noreply.github.com>
2021-12-17 22:00:23 +0900

    画面遷移のアニメーション

--- OG3/Assets/Scenes/Scripts/og3/Moveanimationsclipt.cs
+++ OG3/Assets/Scenes/Scripts/og3/Moveanimationsclipt.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class Moveanimationsclipt : MonoBehaviour
 {
-    [SerializeField] float smallspeed;
+    [SerializeField] float smallspeed = 200;
+    [SerializeField] float bigspeed = 250;
     RectTransform rectTransform;
+    int count;
+    bool bigmove = true;
     // Start is called before the first frame update
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(247.2563f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rectTransform.sizeDelta += new Vector2(0, -smallspeed) * Time.deltaTime;
+        if(rectTransform.sizeDelta.y < 50 && bigmove == true)
+        {
+            rectTransform.sizeDelta += new Vector2(0, +bigspeed) * Time.deltaTime;
+        }
+
+        if(rectTransform.sizeDelta.y > 50)
+        {
+            count++;
+            bigmove = false;
+        }
+
+        if (count > 200)
+        {
+            rectTransform.sizeDelta += new Vector2(0, -smallspeed) * Time.deltaTime;
+        }
+        
         if(rectTransform.sizeDelta.y < 0)
         {
             Destroy(gameObject);
+            count = 0;
+            bigmove = true;
         }
     }
 }
