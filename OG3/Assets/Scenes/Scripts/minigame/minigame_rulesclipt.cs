992ca4ea5319d6e1321051552836c7ba3ec8b66c
Haruka　Karakawa <85378204+427harinn@users.noreply.github.com>
2022-03-04 22:48:57 +0900

    ミニゲームの説明書差し替え

--- OG3/Assets/Scenes/Scripts/minigame/minigame_rulesclipt.cs
+++ OG3/Assets/Scenes/Scripts/minigame/minigame_rulesclipt.cs
@@ -10,6 +10,9 @@ public class minigame_rulesclipt : MonoBehaviour
 {
 
     [SerializeField] GameObject minigamerulePanel1;
+    [SerializeField] GameObject minigamerule;
+    public Sprite minigameruleimage1;
+    public Sprite minigameruleimage2;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,10 +33,18 @@ public class minigame_rulesclipt : MonoBehaviour
     public void onClicked_minigameruleclose()
     {
         minigamerulePanel1.SetActive(false);
+        Image ruleimage = (Image)minigamerule.GetComponent<Image>();
+        ruleimage.sprite = minigameruleimage1;
     }
 
     public void onClicked_returnbutton()
     {
         SceneManager.LoadScene("start scene");
+    }
+
+    public void onClicked_rulenextpagebutton()
+    {
+        Image ruleimage = (Image)minigamerule.GetComponent<Image>();
+        ruleimage.sprite = minigameruleimage2;
     }
 }
